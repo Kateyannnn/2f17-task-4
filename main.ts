b90b222ebf10291a75fbdbdcfa06b58bfb69bd05
Kateyannnn <115198961+Kateyannnn@users.noreply.github.com>
2022-10-21 09:23:30 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,24 +1,31 @@
 input.onButtonPressed(Button.A, function () {
-    xvalue = 0
-    for (let index = 0; index < 5; index++) {
-        led.plot(xvalue, 0)
-        basic.pause(500)
-        basic.clearScreen()
-        xvalue += 1
-    }
+	
 })
 input.onButtonPressed(Button.B, function () {
-    yvalue = 0
-    for (let index = 0; index < 5; index++) {
-        led.plot(0, yvalue)
-        basic.pause(500)
+	
+})
+basic.showString("2F17 Kate")
+let xvalue = 0
+let yvalue = 0
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
         basic.clearScreen()
-        yvalue += 1
+        led.plot(xvalue, 0)
+        basic.pause(500)
+        xvalue += 1
+    }
+    if (xvalue >= 4) {
+        xvalue = 0
     }
 })
-let yvalue = 0
-let xvalue = 0
-basic.showString("2F17 Kate")
 basic.forever(function () {
-	
+    if (input.buttonIsPressed(Button.B)) {
+        basic.clearScreen()
+        led.plot(0, yvalue)
+        basic.pause(500)
+        yvalue += 1
+    }
+    if (yvalue >= 4) {
+        yvalue = 0
+    }
 })
