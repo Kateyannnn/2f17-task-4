acdb465a2492efaa68b8a2dade3537825d7deab4
Kateyannnn <115198961+Kateyannnn@users.noreply.github.com>
2022-10-21 09:40:18 +0800

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -8,24 +8,24 @@ basic.showString("2F17 Kate")
 let xvalue = 0
 let yvalue = 0
 basic.forever(function () {
-    if (input.buttonIsPressed(Button.A)) {
-        basic.clearScreen()
-        led.plot(xvalue, 0)
-        basic.pause(500)
-        xvalue += 1
-    }
-    if (xvalue >= 4) {
-        xvalue = 0
-    }
-})
-basic.forever(function () {
     if (input.buttonIsPressed(Button.B)) {
         basic.clearScreen()
         led.plot(0, yvalue)
         basic.pause(500)
         yvalue += 1
     }
-    if (yvalue >= 4) {
+    if (yvalue > 4) {
         yvalue = 0
+    }
+})
+basic.forever(function () {
+    if (input.buttonIsPressed(Button.A)) {
+        basic.clearScreen()
+        led.plot(xvalue, 0)
+        basic.pause(500)
+        xvalue += 1
+    }
+    if (xvalue > 4) {
+        xvalue = 0
     }
 })
